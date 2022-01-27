02f8ebc7ae8cc28c497e295ae6c7ea17d9e9a3cf
Jonathan Suter <jonathan.suter@envvio.com>
2022-01-26 23:45:59 -0300

    Adjust on UnitOfWork to use transactions

--- WeatherAPI.Core/Common/InfraOperations/IUnitOfWork.cs
+++ WeatherAPI.Core/Common/InfraOperations/IUnitOfWork.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Data;
-using System.Data.Common;
 
 namespace WeatherAPI.Core.Common.InfraOperations
 {
     public interface IUnitOfWork : IDisposable
     {
-        Guid Id { get; }
-        DbConnection Connection { get; }
-        IDbTransaction Transaction { get; }
-        void BeginScope(DbConnection dbConnection);
+        IDbSession _session { get; }
+        void BeginTransaction();
         void Commit();
         void Rollback();
     }
